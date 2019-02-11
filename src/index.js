45d63ced9da42af90f4823711f83f48977d672b0
jamajosa <jhuijgens1@live.nl>
2019-02-11 16:01:09 +0100

    aanpassing

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 const app = require('./app');
+var environment = require('./config/environment');
 
-app.listen(3050,() =>{
-  console.log('running on port 3050');
+var port = process.env.PORT || 8080;
+
+app.listen(environment.env.webPort,() =>{
+  console.log('running on port ' + port );
 });
